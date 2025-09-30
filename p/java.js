060227cd51c578663e69b6dccd6b63b8d994145c
igarashimomoka <igarashimomoka527@gmail.com>
2025-09-30 17:28:34 +0900

    Add files via upload

---
+++ p/java.js
@@ -0,0 +1,13 @@
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('inview'); // 画面に入ったら表示
+      } else {
+        entry.target.classList.remove('inview'); // 画面から出たら非表示に
+      }
+    });
+  });
+
+  document.querySelectorAll('.fade-item').forEach(el => {
+    observer.observe(el);
+  });
